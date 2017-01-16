9bd313e15006beb6ba89c49a2c090c716c637463
zhangyongwei <694498731@qq.com>
2017-01-16 10:59:51 +0800

    Initial commit

---
+++ app/src/main/java/com/atguigu/yingyinxianfeng/fragment/RecyclerViewFragment.java
@@ -0,0 +1,16 @@
+package com.atguigu.yingyinxianfeng.fragment;
+
+import android.view.View;
+
+import com.atguigu.yingyinxianfeng.base.BaseFragment;
+
+/**
+ * Created by 张永卫on 2017/1/16.
+ */
+
+public class RecyclerViewFragment extends BaseFragment {
+    @Override
+    public View initView() {
+        return null;
+    }
+}
